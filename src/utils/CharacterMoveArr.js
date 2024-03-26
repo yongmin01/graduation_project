c09b0019fa7e1ed783f8ff2bf5f4cd6f54f88838
yongmin01 <heart77123@naver.com>
2024-03-26 21:46:42 +0900

    ✨ Feat: 캐릭터 이동 배열 생성

---
+++ src/utils/CharacterMoveArr.js
@@ -0,0 +1,68 @@
+import g0 from "../sources/images/Map/girl0.png";
+import g1 from "../sources/images/Map/girl1.png";
+import g2 from "../sources/images/Map/girl2.png";
+import g3 from "../sources/images/Map/girl3.png";
+import g4 from "../sources/images/Map/girl4.png";
+import g5 from "../sources/images/Map/girl5.png";
+import g6 from "../sources/images/Map/girl6.png";
+import g7 from "../sources/images/Map/girl7.png";
+import g8 from "../sources/images/Map/girl8.png";
+import g9 from "../sources/images/Map/girl9.png";
+import g10 from "../sources/images/Map/girl10.png";
+import g11 from "../sources/images/Map/girl11.png";
+import g12 from "../sources/images/Map/girl12.png";
+import g13 from "../sources/images/Map/girl13.png";
+import g14 from "../sources/images/Map/girl14.png";
+import g15 from "../sources/images/Map/girl15.png";
+import g16 from "../sources/images/Map/girl16.png";
+import g17 from "../sources/images/Map/girl17.png";
+import g18 from "../sources/images/Map/girl18.png";
+import g19 from "../sources/images/Map/girl19.png";
+import g20 from "../sources/images/Map/girl20.png";
+
+const CharacterMoveArr = [
+  g0,
+  g0,
+  g1,
+  g1,
+  g2,
+  g2,
+  g3,
+  g3,
+  g4,
+  g4,
+  g5,
+  g5,
+  g6,
+  g6,
+  g7,
+  g7,
+  g8,
+  g8,
+  g9,
+  g9,
+  g10,
+  g10,
+  g11,
+  g11,
+  g12,
+  g12,
+  g13,
+  g13,
+  g14,
+  g14,
+  g15,
+  g15,
+  g16,
+  g16,
+  g17,
+  g17,
+  g18,
+  g18,
+  g19,
+  g19,
+  g20,
+  g20,
+];
+
+export default CharacterMoveArr;
