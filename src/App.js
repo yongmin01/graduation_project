3ae6a2cf2118dba8a716bf37dff2fe04399abfcf
yongmin01 <heart77123@naver.com>
2024-04-11 01:06:02 +0900

    🔥 Remove: map.jsx,  삭제

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import Intro from "./Pages/Intro";
 import Tutorial from "./Pages/Tutorial";
-import Map from "./Pages/Map";
+
 import Map1 from "./Pages/Map1";
 import Map2 from "./Pages/Map2";
 import Map3 from "./Pages/Map3";
 import Map4 from "./Pages/Map4";
 import MusicQuiz from "./Pages/MusicQuiz";
 import SpeechQuiz from "./Pages/SpeechQuiz";
+import PuzzleQuiz from "./Pages/PuzzleQuiz";
 import Npc1 from "./Pages/Npc1";
 import Npc2 from "./Pages/Npc2";
 import Npc3 from "./Pages/Npc3";
 import Npc4 from "./Pages/Npc4";
-import Test from "./Pages/Test";
-import GuessingQuiz from "./Pages/GuessingQuiz";
-import PuzzleQuiz from "./Pages/PuzzleQuiz";
 
 function App() {
   const handle = useFullScreenHandle();
@@ -36,7 +34,6 @@ function App() {
               element={<Tutorial chooseCharacter={setCharacter} />}
             />
 
-            {/* <Route path="/" element={<Map round={round} end={end} />} /> */}
             <Route path="/map1" element={<Map1 sex={character} />} />
             <Route path="/music" element={<MusicQuiz />} />
             <Route path="/npc1" element={<Npc1 />} />
@@ -51,8 +48,6 @@ function App() {
 
             <Route path="/map4" element={<Map4 sex={character} />} />
             <Route path="/npc4" element={<Npc4 />} />
-
-            <Route path="/guess" element={<GuessingQuiz />} />
           </Routes>
         </BrowserRouter>
         <button onClick={handle.enter}>전체화면 전환</button>
