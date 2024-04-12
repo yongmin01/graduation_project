16d41c2ca58af2039ab19f23578ec77de32b6f76
yongmin01 <heart77123@naver.com>
2024-04-12 15:29:59 +0900

    💄 Design: 배경 테두리 크기 수정

--- src/utils/GameCommonStyle.jsx
+++ src/utils/GameCommonStyle.jsx
@@ -7,15 +7,12 @@ export default function GameCommonStyle({ color }) {
 }
 const GameBgStyle = styled.div`
   background-color: ${(props) => props.color};
-
   background-image: url(${Bg});
-  background-size: 94% 91%;
+  background-size: 94vw 91vh;
   background-repeat: no-repeat;
   background-position: center;
   background-attachment: fixed;
   width: 100vw;
   height: 100vh;
   flex-shrink: 0;
-
-  font-family: "SangSangShinb7";
 `;
