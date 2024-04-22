53cd14bcbe89d9f93e1d05a30e955e17ce636213
yongmin01 <heart77123@naver.com>
2024-04-22 19:21:51 +0900

    ✨ Feat: 음악 퀴즈 문제 랜덤으로 선정

--- src/musicData.js
+++ src/musicData.js
@@ -1,4 +1,10 @@
-export const musics = [
+// totalMusics 배열에서 무작위로 3개의 요소를 선택하는 함수
+function getRandomElements(array, numElements) {
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, numElements);
+}
+
+const totalMusics = [
   {
     id: "5dHcVjj_Kq0",
     title: ["그대에게"],
@@ -29,6 +35,29 @@ export const musics = [
     artist: "SHINee",
     year: "2010",
   },
+  {
+    id: "NrmLcwGjovc",
+    title: ["21세기의 어떤 날", "Some Day in the 21century"],
+    artist: "LUCY",
+    yeart: "2022",
+  },
+  {
+    id: "I1nGGToyte8",
+    title: ["때려쳐"],
+    artist: "DAY6",
+    year: "2020",
+  },
 ];
+
+// totalMusics 배열에서 무작위로 3개의 요소 선택
+const randomMusics = getRandomElements(totalMusics, 5);
+
+// musics 배열에 객체 형태로 담기
+const musics = randomMusics.map(({ id, title, artist, year }) => ({
+  id,
+  title,
+  artist,
+  year,
+}));
 
 export default musics;
