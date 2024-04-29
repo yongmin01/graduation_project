9da0a416c41bd60cc776e90efc23108360a67d4b
yongmin01 <heart77123@naver.com>
2024-04-29 20:33:28 +0900

    ✨ Feat: 엔딩 페이지 제작

--- src/Pages/Diary.jsx
+++ src/Pages/Diary.jsx
@@ -1,21 +1,105 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import page0 from "../sources/images/Outro/diary.png";
-import page1 from "../sources/images/Outro/diary1.webp";
-import page2 from "../sources/images/Outro/diary2.webp";
-import page3 from "../sources/images/Outro/diary3.webp";
-import page4 from "../sources/images/Outro/diary4.webp";
+import styled, { css, keyframes } from "styled-components";
+import bg from "../sources/images/Outro/outroBg.png";
+import cover from "../sources/images/Outro/diaryCover.webp";
+import gPage1 from "../sources/images/Outro/gPage1.png";
+import gPage2 from "../sources/images/Outro/gPage2.png";
+import gPage3 from "../sources/images/Outro/gPage3.png";
+import bPage1 from "../sources/images/Outro/bPage1.png";
+import bPage2 from "../sources/images/Outro/bPage2.png";
+import bPage3 from "../sources/images/Outro/bPage3.png";
+import emptyPage from "../sources/images/Outro/emptyPage.png";
+import coverBack from "../sources/images/Outro/coverBack.svg";
+import pageBack from "../sources/images/Outro/pageBack.svg";
 
 export default function Diary() {
-  const [page, setPage] = useState(0);
+  const characterSex = JSON.parse(localStorage.getItem("character"));
 
-  const nextPage = () => {
-    setPage(page + 1);
+  const [page, setPage] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const [showCover, setShowCover] = useState(true);
+  const [coverFlipped, setCoverFlipped] = useState(false);
+  const [page1Flipped, setPage1Flipped] = useState(false);
+  const [page2Flipped, setPage2Flipped] = useState(false);
+  const [page3Flipped, setPage3Flipped] = useState(false);
+  const [page4Flipped, setPage4Flipped] = useState(false);
+
+  const handleCoverClick = () => {
+    // if (isMoved) {
+    //   // setIsFlipped(!isFlipped);
+    //   setCoverFlipped(true);
+    //   console.log("is Moved! B");
+    // } else if (coverFlipped) {
+    //   setPage1Flipped(true);
+    //   console.log("cover is already Flipped! C");
+    // } else if (page1Flipped) {
+    //   setPage2Flipped(true);
+    //   console.log("page1 is already Flipped! D");
+    // } else if (page2Flipped) {
+    //   setPage3Flipped(true);
+    // } else {
+    //   setIsMoved(true);
+    //   console.log("B");
+    // }
+
+    if (page2Flipped) {
+      setPage3Flipped(true);
+    } else if (page1Flipped) {
+      setPage2Flipped(true);
+    } else if (coverFlipped) {
+      setPage1Flipped(true);
+    } else if (isMoved) {
+      setCoverFlipped(true);
+    }
+    {
+      setIsMoved(true);
+      setTimeout(() => {
+        setShowCover(false);
+      }, 1500);
+    }
   };
 
   return (
-    <Container onClick={nextPage}>
-      <Page src={getPageSource(page)} />
+    <Container>
+      <Cover
+        src={cover}
+        onClick={handleCoverClick}
+        isMoved={isMoved}
+        showCover={showCover}
+      />
+      <Page0
+        src={cover}
+        isMoved={isMoved}
+        showCover={showCover}
+        isFlipped={coverFlipped}
+        afterPage={page1Flipped}
+        onClick={handleCoverClick}
+      />
+      <Page1
+        src={characterSex === "girl" ? gPage1 : bPage1}
+        beforePage={coverFlipped}
+        isFlipped={page1Flipped}
+        afterPage={page2Flipped}
+        onClick={handleCoverClick}
+      />
+
+      <Page
+        src={characterSex === "girl" ? gPage2 : bPage2}
+        beforePage={page1Flipped}
+        isFlipped={page2Flipped}
+        afterPage={page3Flipped}
+        onClick={handleCoverClick}
+      />
+
+      <Page
+        src={characterSex === "girl" ? gPage3 : bPage3}
+        beforePage={page2Flipped}
+        isFlipped={page3Flipped}
+        afterPage={page4Flipped}
+        onClick={handleCoverClick}
+      />
+      <Page4 src={emptyPage} beforePage={page3Flipped} />
+      {/* <Page src={page1} isMoved={isMoved} onClick={handleCoverClick} /> */}
     </Container>
   );
 }
@@ -23,29 +107,124 @@ export default function Diary() {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+  background: url(${bg}) no-repeat center fixed;
+  background-size: cover;
+  perspective: 1000px;
+  position: relative;
 `;
 
+const Cover = styled.img`
+  position: absolute;
+  left: 33vw;
+  top: 9.2vh;
+  height: 81vh;
+  opacity: ${({ showCover }) => (showCover ? "1" : "0")};
+  transition: transform 1.5s ease-in-out, opacity 1.5s ease-in-out;
+  transform-origin: left center;
+  ${({ isMoved }) =>
+    isMoved &&
+    css`
+      transform: translateX(14.5vw);
+    `}
+  z-index: 2;
+`;
+
+const Page0 = styled.img`
+  position: absolute;
+  left: 47.8vw;
+  top: 9.2vh;
+  height: 81vh;
+  display: ${({ showCover }) => (showCover ? "none" : "flex")};
+  transition: transform 1.5s ease-in-out, opacity 1.5s ease-in-out;
+  transform-origin: left center;
+  ${({ isFlipped, afterPage }) =>
+    isFlipped &&
+    css`
+      transform: rotateY(-180deg);
+      opacity: ${afterPage ? 0 : isFlipped ? 0.5 : 1};
+    `}
+  z-index: 1;
+`;
+
+const Page1 = styled.img`
+  position: absolute;
+  left: 47.8vw;
+  top: 9.2vh;
+  height: 81vh;
+  display: ${({ beforePage }) => (beforePage ? "flex" : "none")};
+  transition: transform 1.5s ease-in-out, opacity 1.5s ease-in-out;
+  transform-origin: left center;
+  ${({ isFlipped, afterPage }) =>
+    isFlipped &&
+    css`
+      transform: rotateY(-180deg);
+      opacity: ${afterPage ? 0 : isFlipped ? 0.5 : 1};
+    `}
+  z-index: ${({ isFlipped }) => (isFlipped ? "1" : "-1")};
+`;
 const Page = styled.img`
-  object-fit: contain;
+  position: absolute;
+  left: 47.8vw;
+  top: 9.2vh;
+  height: 81vh;
+  display: ${({ beforePage }) => (beforePage ? "flex" : "none")};
+  transition: transform 1.5s ease-in-out, opacity 1.5s ease-in-out;
+  transform-origin: left center;
+  ${({ isFlipped, afterPage }) =>
+    isFlipped &&
+    css`
+      transform: ${isFlipped ? "rotateY(-180deg)" : "translateX(556px)"};
+      opacity: ${afterPage ? 0 : isFlipped ? 0.5 : 1};
+    `}
+  z-index: ${({ isFlipped }) => (isFlipped ? "1" : "-1")};
+`;
+const Page4 = styled.img`
+  position: absolute;
+  left: 47.8vw;
+  top: 9.2vh;
+  height: 81vh;
+  display: ${({ beforePage }) => (beforePage ? "flex" : "none")};
 `;
 
-const getPageSource = (page) => {
-  switch (page) {
-    case 0:
-      return page0;
-    case 1:
-      return page1;
-    case 2:
-      return page2;
-    case 3:
-      return page3;
-    case 4:
-      return page4;
-    default:
-      return page0;
-  }
-};
+// const PageImg = styled.img`
+//   width: 100%;
+//   height: 100%;
+// `;
+// const Front = styled.div`
+//   position: absolute;
+//   top: 0px;
+//   left: 0px;
+//   width: 100%;
+//   height: 100%;
+//   transition: all 2s;
+//   backface-visibility: hidden;
+//   background-color: yellow;
+//   z-index: 2;
+//   transform-origin: left center;
+//   ${({ isFlipped }) =>
+//     isFlipped &&
+//     css`
+//       z-index: 1;
+//       transform: rotateY(-180deg);
+//     `}
+// `;
+
+// const Back = styled.div`
+//   position: absolute;
+//   top: 0px;
+//   left: 0px;
+//   width: 100%;
+//   height: 100%;
+//   transition: all 2s;
+//   backface-visibility: hidden;
+//   z-index: 1;
+//   transform: rotateY(180deg);
+//   transform-origin: left center;
+
+//   ${({ isFlipped }) =>
+//     isFlipped &&
+//     css`
+//       z-index: 2;
+//       transform: rotateY(0deg);
+//     `}
+// `;
