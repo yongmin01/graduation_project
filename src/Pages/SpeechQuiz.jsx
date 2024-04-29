442f5ff495a1f7340ba335773eea4f745bfe193e
yongmin01 <heart77123@naver.com>
2024-04-29 20:36:35 +0900

    💄 Design: 방송 퀴즈 UI 변경

--- src/Pages/SpeechQuiz.jsx
+++ src/Pages/SpeechQuiz.jsx
@@ -6,19 +6,23 @@ import BeforeGame from "../Components/BeforeGame";
 import GameResult from "../Components/GameResult";
 import Chance from "../Components/Chance";
 import speech from "../speechData";
-import { ReactComponent as RecordBtn } from "../sources/images/Game/recordBtn.svg";
-import { ReactComponent as PlayIcon } from "../sources/images/Game/playIcon.svg";
-import { ReactComponent as InputPath } from "../sources/images/Game/inputPath.svg";
-import startBtn from "../sources/images/Game/startBtn.svg";
-import stopBtn from "../sources/images/Game/stopBtn.svg";
-import { useNavigate } from "react-router-dom";
+import playIcon from "../sources/images/Game/playIcon_blue.svg";
+
+import startBtn from "../sources/images/Game/speechQuiz/startBtn.svg";
+import stopBtn from "../sources/images/Game/speechQuiz/stopBtn.svg";
+
+import correctImg from "../sources/images/Game/correct.svg";
+import wrongImg from "../sources/images/Game/wrong.svg";
 
 export default function SpeechQuiz({}) {
+  const totalQuiz = 3;
   const [game, setGame] = useState("before");
   const [nowPlaying, setNowPlaying] = useState(false);
   const [quizIndex, setQuizIndex] = useState(0);
+  const [showPass, setShowPass] = useState(false);
   const [roundEnd, setRoundEnd] = useState(false);
   const [userAnswer, setUserAnswer] = useState(null);
+  const [isFirstTry, setIsFirstTry] = useState(true);
   const [chance, setChance] = useState(2);
   const [correct, setCorrect] = useState();
   const [endAlert, setEndAlert] = useState(false);
@@ -34,6 +38,12 @@ export default function SpeechQuiz({}) {
     },
   });
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowPass(true);
+    }, 5000);
+  }, [quizIndex]);
+
   // 문제 관련 로직
   const play = () => {
     playerRef.current.currentTime = 0;
@@ -48,13 +58,18 @@ export default function SpeechQuiz({}) {
       playerRef.current.pause();
     }
   };
+  const handleFirstTry = () => {
+    listen();
+    setIsFirstTry(false);
+  };
   const next = () => {
-    if (quizIndex === 3 - 1) {
+    if (quizIndex === totalQuiz - 1) {
       setGame("end");
     }
     setRoundEnd(false);
-    setUserAnswer("");
+    setUserAnswer(null);
     setQuizIndex(quizIndex + 1);
+    setIsFirstTry(true);
     setNowPlaying(false);
     setChance(2);
   };
@@ -72,13 +87,23 @@ export default function SpeechQuiz({}) {
   }, [roundEnd]);
 
   useEffect(() => {
-    if (quizIndex === 3 - 1 && roundEnd) {
+    if (quizIndex === totalQuiz - 1 && roundEnd) {
       setTimeout(() => {
         setEndAlert(true);
       }, 3000);
     }
   }, [quizIndex, roundEnd]);
 
+  const handlePass = () => {
+    if (listening) stop();
+    if (nowPlaying) setNowPlaying(false);
+    setCorrect(false);
+    setShowPass(false);
+    setRoundEnd(true);
+    setTimeout(() => {
+      setCorrect(null);
+    }, 2000);
+  };
   const handleEnd = () => {
     if (score.current >= 2) {
       pass.current = true;
@@ -98,25 +123,25 @@ export default function SpeechQuiz({}) {
   };
   const clickSubmit = () => {
     if (userAnswer === null) return;
+    stop();
     if (!playerRef.current.paused) {
       playerRef.current.pause();
     }
     if (normalization(userAnswer) === normalization(speech[quizIndex].answer)) {
-      setCorrect("정답입니다!^ㅇ^");
+      setCorrect(true);
       score.current = score.current + 1;
       setRoundEnd(true);
-      stop();
+      setShowPass(false);
       setTimeout(() => {
         setCorrect(null);
       }, 650);
     } else {
-      setCorrect("오답입니다! -.-");
+      setCorrect(false);
       setTimeout(() => {
         setCorrect(null);
       }, 900);
       setChance(chance - 1);
       setUserAnswer("");
-      console.log("틀림");
     }
   };
   function makeBlank(answer) {
@@ -140,96 +165,147 @@ export default function SpeechQuiz({}) {
       <GameCommonStyle color={"#CEEAFF"} />
       {game === "start" ? (
         <Game>
-          {/* 남은 기회 */}
-          <Chance total="2" remaining={chance} />
-          {/* 문제 진행도 */}
-          <Progress>{quizIndex + 1}/3</Progress>
-          <QuizIndex>문제 {quizIndex + 1}</QuizIndex>
-          {roundEnd ? (
-            quizIndex !== 2 ? (
-              <NextBtn onClick={next}>
-                <PlayIcon />
-              </NextBtn>
-            ) : null
-          ) : null}
-          <QuizDiv>
-            <Player>
-              {/* <Cover onClick={replay} /> */}
-              {/* <Hider> */}
-              <video
-                src={`/videos/speechQuiz${quizIndex}.mp4#t,${speech[quizIndex].end}`}
-                ref={playerRef}
-                onTimeUpdate={pauseVideo}
-                onClick={replay}
-              />
-            </Player>
-            {correct != null ? (
-              <Correct
-                color={correct === "정답입니다!^ㅇ^" ? "#13BF33" : "#FF3E3E"}
-                bgcolor={correct === "정답입니다!^ㅇ^" ? "#F5FFF5" : "#FFF5F5"}
-                shadowcolor={
-                  correct === "정답입니다!^ㅇ^"
-                    ? "127, 191, 139, 0.25"
-                    : "255, 62, 62, 0.20"
-                }
-              >
-                {correct}
-              </Correct>
-            ) : roundEnd ? (
-              <AnswerInfo>
-                <span>
-                  {speech[quizIndex].quiz !== undefined
-                    ? speech[quizIndex].quiz + " " + speech[quizIndex].answer
-                    : speech[quizIndex].answer}
-                </span>
-                {/* <span>
-                  {speech[quizIndex].title}-{speech[quizIndex].year}
-                </span> */}
-              </AnswerInfo>
-            ) : (
+          <Container>
+            <Header>
+              {/* 남은 기회 */}
+              <Chance total="2" remaining={chance} />
+              <Center>
+                {/* 문제 진행도 */}
+                <Progress>
+                  {quizIndex + 1}/{totalQuiz}
+                </Progress>
+                <QuizIndex>문제 {quizIndex + 1}</QuizIndex>
+              </Center>
+
+              {showPass ? (
+                <NextBtn onClick={handlePass} show={showPass}>
+                  <span>PASS</span>
+                  <img src={playIcon} />
+                </NextBtn>
+              ) : null}
+
+              {showPass ? null : (
+                <NextBtn
+                  onClick={next}
+                  show={roundEnd && quizIndex !== totalQuiz - 1}
+                >
+                  <span>다음 문제</span>
+                  <img src={playIcon} />
+                </NextBtn>
+              )}
+            </Header>
+            <QuizDiv isFirstTry={isFirstTry}>
               <Quiz>
-                {speech[quizIndex].quiz !== undefined
-                  ? speech[quizIndex].quiz +
-                    " " +
-                    makeBlank(speech[quizIndex].answer)
-                  : makeBlank(speech[quizIndex].answer)}
+                <Player>
+                  <Video
+                    src={`/videos/speechQuiz${quizIndex}.mp4#t,${speech[quizIndex].end}`}
+                    ref={playerRef}
+                    onTimeUpdate={pauseVideo}
+                    onClick={replay}
+                  />
+                </Player>
+                {roundEnd ? (
+                  <AnswerInfo>
+                    <span>
+                      {speech[quizIndex].quiz !== undefined
+                        ? speech[quizIndex].quiz +
+                          " " +
+                          speech[quizIndex].answer
+                        : speech[quizIndex].answer}
+                    </span>
+                  </AnswerInfo>
+                ) : (
+                  <AnswerInfo>
+                    {speech[quizIndex].quiz !== undefined
+                      ? speech[quizIndex].quiz +
+                        " " +
+                        makeBlank(speech[quizIndex].answer)
+                      : makeBlank(speech[quizIndex].answer)}
+                  </AnswerInfo>
+                )}
               </Quiz>
-            )}
-          </QuizDiv>
-          <InputDiv>
-            <div>답</div>
-            <InputBox>
-              <InputTagBox
-                ref={inputRef}
-                value={userAnswer || ""}
-                onChange={(e) => setUserAnswer(e.target.value)}
-                disabled
-              ></InputTagBox>
-              <InputPath />
-            </InputBox>
-            <SubmitDiv>
-              <SubmitBtn
-                onClick={clickSubmit}
-                disabled={nowPlaying || roundEnd}
-              >
-                제출
-              </SubmitBtn>
-            </SubmitDiv>
-          </InputDiv>
-          <Btn onClick={listening ? stop : listen}>
-            <BtnContent>
-              {listening ? <img src={stopBtn} /> : <img src={startBtn} />}
-              <span>{listening ? "녹음 완료" : "녹음하기"}</span>
-            </BtnContent>
-            <RecordBtn width="14vw" height="8.7vh" fill="#E7F5FF" />
-          </Btn>
+
+              <InputDiv>
+                {isFirstTry ? (
+                  <Btn
+                    onClick={handleFirstTry}
+                    isFirstTry={isFirstTry}
+                    disabled={roundEnd}
+                  >
+                    <BtnImg src={startBtn} />
+                    <span>답안 녹음하기</span>
+                  </Btn>
+                ) : (
+                  <RecordingInput>
+                    <InputBox>
+                      <InputTagBox
+                        ref={inputRef}
+                        value={userAnswer || ""}
+                        onChange={(e) => setUserAnswer(e.target.value)}
+                        disabled
+                      ></InputTagBox>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="482"
+                        height="2"
+                        viewBox="0 0 482 2"
+                        fill="none"
+                      >
+                        <path
+                          d="M1 1L481 1.00004"
+                          stroke="#143165"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-dasharray="4 4"
+                        />
+                      </svg>
+                    </InputBox>
+                    <BtnDiv>
+                      {listening ? (
+                        <Btn
+                          onClick={stop}
+                          isFirstTry={isFirstTry}
+                          disabled={roundEnd}
+                        >
+                          <BtnImg src={stopBtn} />
+                          <span>녹음 끝내기</span>
+                        </Btn>
+                      ) : (
+                        <Btn
+                          onClick={listen}
+                          isFirstTry={isFirstTry}
+                          disabled={roundEnd}
+                        >
+                          <BtnImg src={startBtn} />
+                          <span>다시 녹음하기</span>
+                        </Btn>
+                      )}
+
+                      <Btn
+                        onClick={clickSubmit}
+                        isFirstTry={isFirstTry}
+                        disabled={roundEnd}
+                      >
+                        답안 제출하기
+                      </Btn>
+                    </BtnDiv>
+                  </RecordingInput>
+                )}
+              </InputDiv>
+            </QuizDiv>
+          </Container>
+          {correct === true ? (
+            <Correct src={correctImg} />
+          ) : correct === false ? (
+            <Correct src={wrongImg} />
+          ) : null}
           {endAlert ? (
             <EndAlert onClick={handleEnd}>게임 종료!</EndAlert>
           ) : null}
         </Game>
       ) : game === "before" ? (
         <>
-          <BeforeGame go={setGame} title="대사 이어말하기" round={2} />
+          <BeforeGame go={setGame} title="대사 이어말하기" round={3} />
         </>
       ) : game === "end" ? (
         <GameResult
@@ -252,66 +328,89 @@ const Game = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7.3vh;
+  align-items: start;
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 18.8vw;
+  margin-right: ${({ showPass }) => (showPass ? "16.9vw" : "14.5vw")};
+`;
 const Progress = styled.div`
   font-size: 3.5vh;
-  font-family: UhBeeJung;
+  font-family: "UhBeeJung";
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   color: #151b26;
 `;
 const QuizIndex = styled.div`
   font-size: 8.7vh;
-  font-family: UhBeeJung;
+  font-family: "UhBeeJung";
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   color: #151b26;
   margin-bottom: 3.3vh;
 `;
+
+const QuizDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-right: ${({ isFirstTry }) => (isFirstTry ? "4.79vw" : "auto")};
+  position: relative;
+`;
+const Quiz = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.9vh;
+  color: #151b26;
+  text-align: center;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: "Gaegu";
+  font-size: 2vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
 const Player = styled.div`
-  width: 58vh;
-  height: 29vh;
   display: flex;
   position: relative;
   justify-content: center;
 `;
-const Cover = styled.div`
-  height: 29vh;
-  position: absolute;
+const Video = styled.video`
+  height: 34.1vh;
+`;
+const AnswerInfo = styled.div`
+  font-family: "Gaegu";
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-const Btn = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  position: relative;
-  font-family: Gaegu;
-  font-size: 30px;
-  width: 14vw;
-  height: 8.7vh;
-`;
-const BtnContent = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 5px;
-  position: absolute;
-  top: 5px;
-  width: 14vw;
-  height: 8.7vh;
-`;
-const NextBtn = styled.button`
-  border: none;
-  background: transparent;
-  position: absolute;
-  right: 9.5vw;
-  top: 2vh;
-`;
 const InputDiv = styled.div`
   font-family: "UhbeeJung";
   font-size: 50px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   margin-bottom: 2.25vh;
+`;
+const RecordingInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6.8vh;
 `;
 const InputBox = styled.div`
   display: flex;
@@ -326,67 +425,62 @@ const InputTagBox = styled.input`
   font-size: 44px;
   margin-bottom: 4px;
 `;
-const SubmitDiv = styled.div`
-  position: relative;
+const BtnDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
 `;
-const SubmitBtn = styled.button`
-  border: none;
+const Btn = styled.button`
+  width: ${({ isFirstTry }) => (isFirstTry ? "22.5vw" : "16.5vw")};
+  height: ${({ isFirstTry }) => (isFirstTry ? "9.3vw" : "6vw")};
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  justify-content: center;
+  align-items: center;
   background-color: transparent;
   font-family: "UhbeeJung";
-  font-size: 50px;
+  font-size: ${({ isFirstTry }) => (isFirstTry ? "2.3vw" : "1.9vw")};
   color: #151b26;
-  &:before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 80%;
-    height: 60%;
-    background-color: rgba(27, 127, 199, 0.371);
-    filter: blur(15px);
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  ${SubmitDiv}:hover &:before {
-    opacity: 1;
-  }
+  border-radius: ${({ isFirstTry }) => (isFirstTry ? "4.1vw" : "2.7vw")};
+  border: 0.4px solid #3c7bac;
+  background: #eff8ff;
+  box-shadow: 2px 4px 4px 0px rgba(0, 81, 155, 0.25);
+  background: #eff8ff;
+`;
+const BtnImg = styled.img`
+  width: 0.8vw;
+  height: 0.8vw;
 `;
 
-const Correct = styled.div`
-  width: 884px;
-  height: 8.3vh;
-  border-radius: 10px;
-  color: ${(props) => props.color};
-  background: ${(props) => props.bgcolor};
-  box-shadow: 0px 4px 10px 0px rgba(${(props) => props.shadowcolor});
-  margin-bottom: 1.5vh;
-  font-family: "Gaegu";
-  font-size: 50px;
+const Correct = styled.img`
+  width: 65.2vh;
+  height: 65.2vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const NextBtn = styled.button`
+  width: 16.4vw;
+  height: 7vh;
+  visibility: ${({ show }) => (show ? "visible" : "hidden")};
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1vw;
+  color: #151b26;
   text-align: center;
-`;
-const AnswerInfo = styled.div`
-  height: 8.3vh;
-  font-family: "Gaegu";
-  display: flex;
-  align-items: center;
-  font-size: 30px;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const Quiz = styled.span`
-  font-size: 30px;
-  font-family: Gaegu;
-`;
-
-const QuizDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  height: 35vh;
-  margin-bottom: 0.5vh;
+  font-family: "UhBee jung BOLD";
+  font-size: 2.7vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  border: none;
+  background: transparent;
 `;
 
 const EndAlert = styled.div`
