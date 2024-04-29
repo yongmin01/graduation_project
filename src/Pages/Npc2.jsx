88faa55d6408f327e9d5fb509c8ac6dd00600ae5
yongmin01 <heart77123@naver.com>
2024-04-29 20:39:01 +0900

    💄 Design: npc 페이지 UI 수정

--- src/Pages/Npc2.jsx
+++ src/Pages/Npc2.jsx
@@ -1,47 +1,76 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+import { ReactComponent as NextBtnImg } from "../sources/images/nextBtn.svg";
 import afterGameMap from "../sources/images/Map/map2/afterGame.png";
-import nextBtnImage from "../sources/images/nextBtn.svg";
+import ItemImage from "../sources/images/Game/jetty.svg";
+import getDiaryImg from "../sources/images/getDiary.svg";
 
 export default function Npc2() {
   const videoRef = useRef();
-  const [videoStart, setVideoStart] = useState(false);
   const [videoEnd, setVideoEnd] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [showNext, setShowNext] = useState(false);
 
   const navigator = useNavigate();
   const routeing = () => {
-    setInterval(() => {
+    setTimeout(() => {
       navigator("/map3");
     }, 1000);
   };
 
-  const playVideo = () => {
+  const useItem = () => {
+    setButtonClicked(true);
     if (videoRef.current) {
-      setTimeout(() => {
-        videoRef.current.play();
-      }, 300);
-      setVideoStart(true);
+      videoRef.current.play();
+      videoRef.current.addEventListener("ended", () => {
+        setVideoEnd(true);
+      });
     }
   };
+
+  useEffect(() => {
+    if (videoEnd) {
+      setTimeout(() => {
+        setShowNext(true);
+      }, 2000);
+    }
+  }, [videoEnd]);
 
   return (
     <>
       <Npc>
         <Modal>
-          {videoEnd ? (
-            <Button style={{ display: "flex" }}>
-              <Text onClick={routeing}>다음 맵으로 이동</Text>
-              <img src={nextBtnImage} />
-            </Button>
-          ) : null}
-          {videoStart ? null : (
-            <UseItem onClick={playVideo}>아이템 사용하기</UseItem>
-          )}
-          <Video ref={videoRef} onEnded={() => setVideoEnd(true)}>
+          <Video
+            ref={videoRef}
+            style={{ display: buttonClicked ? "block" : "none" }}
+          >
             <source src="/videos/npc_jetty.mov" />
           </Video>
+
+          <ItemUseBtn
+            onClick={useItem}
+            style={{ display: buttonClicked ? "none" : "flex" }}
+          >
+            <Item>
+              <ItemImg src={ItemImage} />
+              <span style={{ fontSize: "3.5vh" }}>제티</span>
+            </Item>
+            <UseText>사용하기</UseText>
+          </ItemUseBtn>
+          {videoEnd && (
+            <GetDiary>
+              <GetDiaryAlert src={getDiaryImg} />
+              <Button
+                onClick={routeing}
+                style={{ display: "flex", marginLeft: "auto" }}
+                show={showNext}
+              >
+                <Text>다음 맵으로 이동</Text>
+                <NextBtnImg width="2.6vw" fill={"#151B26"} />
+              </Button>
+            </GetDiary>
+          )}
         </Modal>
       </Npc>
     </>
@@ -71,11 +100,78 @@ const Modal = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const ItemUseBtn = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  color: #000;
+  font-family: "UhBee jung";
+  font-size: 7.8vh;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+const Item = styled.div`
+  height: 26vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 3.4vw;
+`;
+const ItemImg = styled.img`
+  height: 20vh;
+`;
+const UseText = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: "UhBee jung";
+  font-size: 5.5vw;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-10%, -50%);
+    width: 19.4vw;
+    height: 2.5vh;
+    background-color: #adff90;
+    border-radius: 20px;
+    filter: blur(20px);
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+
+  &:hover:before {
+    opacity: 1;
+  }
+`;
+const GetDiary = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 41px;
+`;
+const GetDiaryAlert = styled.img`
+  height: 54.2vh;
+`;
 const Video = styled.video`
   width: 54vw;
   height: 57vh;
 `;
 const Button = styled.div`
+  visibility: ${({ show }) => (show ? "visible" : "hidden")};
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -85,17 +181,17 @@ const Button = styled.div`
   top: 6.7%;
   right: 4.6%;
   font-size: 50px;
-  font-family: UhbeeJung;
+  font-family: "UhbeeJung";
   cursor: pointer;
+  z-index: 100;
 `;
-const UseItem = styled.div`
-  position: absolute;
-  margin: 0 auto;
-  width: 40%;
-  background-color: yellow;
-  border-radius: 50px;
-  text-align: center;
-  font-family: Uhbee Jung;
-  z-index: 10;
+
+const Text = styled.div`
+  color: #000;
+  text-align: right;
+  font-family: "UhBee jung";
+  font-size: 4.2vh;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
-const Text = styled.div``;
--- src/Pages/Npc4.jsx
+++ src/Pages/Npc4.jsx
@@ -1,28 +1,30 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+import { ReactComponent as NextBtnImg } from "../sources/images/nextBtn.svg";
 import afterGameMap from "../sources/images/Map/map4/afterGame.png";
 import nextBtnImage from "../sources/images/nextBtn.svg";
+import ItemImage from "../sources/images/Game/npc4_play.png";
 
 export default function Npc4() {
   const videoRef = useRef();
-  const [videoStart, setVideoStart] = useState(false);
   const [videoEnd, setVideoEnd] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
 
   const navigator = useNavigate();
   const routeing = () => {
-    setInterval(() => {
-      navigator("/map2");
+    setTimeout(() => {
+      navigator("/diary");
     }, 1000);
   };
 
-  const playVideo = () => {
+  const useItem = () => {
+    setButtonClicked(true);
     if (videoRef.current) {
-      setTimeout(() => {
-        videoRef.current.play();
-      }, 300);
-      setVideoStart(true);
+      videoRef.current.play();
+      videoRef.current.addEventListener("ended", () => {
+        setVideoEnd(true);
+      });
     }
   };
 
@@ -30,18 +32,26 @@ export default function Npc4() {
     <>
       <Npc>
         <Modal>
-          {videoEnd ? (
-            <Button style={{ display: "flex" }}>
-              <Text onClick={routeing}>다음 맵으로 이동</Text>
-              <img src={nextBtnImage} />
-            </Button>
-          ) : null}
-          {videoStart ? null : (
-            <UseItem onClick={playVideo}>아이템 사용하기</UseItem>
-          )}
-          <Video ref={videoRef} onEnded={() => setVideoEnd(true)}>
-            <source src="/videos/npc_tv.mp4" />
+          <Video
+            ref={videoRef}
+            // style={{ display: buttonClicked ? "block" : "none" }}
+          >
+            <source src="/videos/npc_tv.mov" />
           </Video>
+
+          <ItemUseBtn
+            onClick={useItem}
+            style={{ display: buttonClicked ? "none" : "flex" }}
+          >
+            <span>Play</span>
+            <img src={ItemImage} width="40%" style={{ zIndex: "100" }} />
+          </ItemUseBtn>
+          {videoEnd && (
+            <Button onClick={routeing}>
+              <Text>이동</Text>
+              <NextBtnImg width="2.6vw" fill={"#151B26"} />
+            </Button>
+          )}
         </Modal>
       </Npc>
     </>
@@ -71,6 +81,30 @@ const Modal = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+const ItemUseBtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  margin: 0 auto;
+  width: 54vw;
+  height: 57vh;
+  text-align: center;
+  font-family: Uhbee Jung;
+  z-index: 10;
+  position: absolute;
+  background-color: rgba(255, 255, 255, 0.737);
+
+  color: #000;
+  text-align: center;
+  font-family: "UhBee jung";
+  font-size: 2.7vw;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 const Video = styled.video`
   width: 54vw;
   height: 57vh;
@@ -87,7 +121,21 @@ const Button = styled.div`
   font-size: 50px;
   font-family: UhbeeJung;
   cursor: pointer;
+  z-index: 100;
+  width: auto;
 `;
+
+const Text = styled.div`
+  color: #000;
+  text-align: right;
+  font-family: "UhBee jung";
+  font-size: 4.2vh;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  white-space: nowrap;
+`;
+
 const UseItem = styled.div`
   position: absolute;
   margin: 0 auto;
@@ -98,4 +146,3 @@ const UseItem = styled.div`
   font-family: Uhbee Jung;
   z-index: 10;
 `;
-const Text = styled.div``;
